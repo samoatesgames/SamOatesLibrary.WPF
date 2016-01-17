c936c24b1722de256450ae9eb38f7da54be1cebb
samoatesgames <sam@samoatesgames.com>
2016-01-17 14:10:14 +0000

    Add a ViewModelBase and a DelegateCommand class.

---
+++ Command/DelegateCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SamOatesLibrary.WPF.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> m_canExecute;
+        private readonly Action<object> m_execute;
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            m_execute = execute;
+            m_canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Check to see if the command can be executed
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            if (m_canExecute == null)
+            {
+                return true;
+            }
+
+            return m_canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Execute the delegate command
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            m_execute(parameter);
+        }
+
+        /// <summary>
+        /// Raise can execute event
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamOatesLibrary.WPF.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <param name="property"></param>
+        protected void SetAndNotify<T>(ref T source, T value, Expression<Func<T>> property)
+        {
+            var propertyInfo = ((MemberExpression)property.Body).Member as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException("The lambda expression 'property' should point to a valid Property");
+            }
+
+            SetAndNotify(ref source, value, propertyInfo.Name);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        protected void SetAndNotify<T>(ref T source, T value, String propertyName)
+        {
+            if (!EqualityComparer<T>.Default.Equals(source, value))
+            {
+                source = value;
+                RaisePropertyChangedNotifaction(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Raise a property changed event
+        /// </summary>
+        /// <param name="property">The name of the property to rasie an event for</param>
+        protected void RaisePropertyChangedNotifaction(String property)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
